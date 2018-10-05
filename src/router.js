3ebece1aa1e911ecf6c9e485b852f9609ed7fdbd
Callan <cerveauxallanjean@gmail.com>
2018-10-05 16:36:20 +0200

    modify home route

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {
-      path: "/projects",
+      path: "/",
       name: "projects",
       component: () => import("./views/Projects.vue")
     }
