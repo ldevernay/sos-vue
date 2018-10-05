e1accb1ca31580e71c0babb04b1d033a31aee286
Callan <cerveauxallanjean@gmail.com>
2018-10-05 16:50:15 +0200

    add v-btn-toggle for create double btn & add icon on title

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -4,14 +4,24 @@
 			<v-flex xs4 v-for="project in projects">
 				<v-card :key="project.title" color="blue-grey darken-2" class="white--text">
 					<v-card-title primary-title>
-						<h3 class="headless">{{ project.name }}</h3>
+						<h3 class="headless">
+							<v-icon class="white--text">fas {{ project.icon }}</v-icon>
+							{{ project.name }}
+						</h3>
 					</v-card-title>
 					<v-card-actions>
 						<section v-for="chip in project.tags">
 							<v-chip>{{ chip }}</v-chip>
 						</section>
 						<v-spacer></v-spacer>
-						<v-btn :href="`https://github.com/${project.link}`"><v-icon>fab fa-github</v-icon></v-btn>
+						<v-btn-toggle>
+							<v-btn :href="project.demo">
+								<v-icon>fas fa-globe</v-icon>
+							</v-btn>
+							<v-btn :href="`https://github.com/${project.link}`">
+								<v-icon>fab fa-github</v-icon>
+							</v-btn>
+						</v-btn-toggle>
 					</v-card-actions>
 				</v-card>
 			</v-flex>
