b687a6998066fc5608c9c12a4ebb451d74efb9d4
Callan <cerveauxallanjean@gmail.com>
2018-10-05 16:28:31 +0200

    Create view and component for Project, import all data in json file display this data

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,33 @@
+<template>
+	<v-container grid-list-md text-xs-center>
+		<v-layout row wrap >
+			<v-flex xs4 v-for="project in projects">
+				<v-card :key="project.title" color="blue-grey darken-2" class="white--text">
+					<v-card-title primary-title>
+						<h3 class="headless">{{ project.name }}</h3>
+					</v-card-title>
+					<v-card-actions>
+						<section v-for="chip in project.tags">
+							<v-chip>{{ chip }}</v-chip>
+						</section>
+						<v-spacer></v-spacer>
+						<v-btn :href="`https://github.com/${project.link}`"><v-icon>fab fa-github</v-icon></v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-flex>
+		</v-layout>
+	</v-container>
+</template>
+
+<script>
+	export default {
+		props: {
+			projects_list: Object
+		},
+		data(){
+			return {
+				projects: this.projects_list.projects
+			}
+		}
+	}
+</script>
---
+++ src/views/Projects.vue
@@ -0,0 +1,19 @@
+<template>
+	<project-list :projects_list="{projects}"></project-list>
+</template>
+
+<script>
+	import ProjectList from '@/components/ProjectList'
+	import projects from '@/projects.json'
+
+	export default {
+		components: {
+			ProjectList
+		},
+		data(){
+			return {
+				projects: projects
+			}
+		},
+	}
+</script>
