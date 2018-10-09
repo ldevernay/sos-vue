599bdc2b24f3e174df63c7614db451978f7c5346
ldevernay <boblehamster@yahoo.fr>
2018-10-09 16:24:27 +0200

    display raw github relevant data

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -2,8 +2,16 @@
 		<section>
 			<h1>Simplon Open-Source</h1>
 			<h2>{{this.project.name}}</h2>
-			<div>{{this.project}}</div>
-			<div>{{this.github_data}}</div>
+			<div>Tags : {{this.project.tags}}</div>
+			<div>Online demo : {{this.project.demo}}</div>
+			<div>URL : {{this.github_data.html_url}}</div>
+			<div>Description : {{this.github_data.description}}</div>
+			<div>Stars : {{this.github_data.stargazers_count}}</div>
+			<div>Watch : {{this.github_data.watchers_count}}</div>
+			<div>Languages : {{this.github_data.languages}}</div>
+			<div>Forks : {{this.github_data.forks_count}}</div>
+			<div>Issues : {{this.github_data.open_issues_count}}</div>
+			<div>License : {{this.github_data.license}}</div>
 			<router-link to="/" class="link">Retour à la liste</router-link>
 		</section>
 	</template>
@@ -25,10 +33,16 @@
 				this.project = this.projects.filter(val => val.id == this.$route.params.project_id)[0];
 				// Fetching data from github API for current project
 				fetch(`https://api.github.com/repos/${this.project.link}`, {
-			method: 'GET'
-		})
-			.then(response => response.json())
-			.then(json => (this.github_data = json));
+				method: 'GET'
+				})
+				.then(response => response.json())
+				.then(json => (this.github_data = json));
+				// Fetching contributors data from github API for current project
+				// fetch(`https://api.github.com/repos/${this.project.link}/contributors`, {
+				// method: 'GET'
+				// })
+				// .then(response => response.json())
+				// .then(json => (this.contributors_data = json));
 			}
 		}
 	</script>
