c5a06262ad7cfda156a5d4485e108fa31d68cb6b
Orel <aurelien.chirot@idee-lab.fr>
2018-12-21 16:04:33 +0100

    adding WIP filter

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,5 +1,11 @@
 <template>
 <section>
+      <v-switch
+        :label="`production projets only (no WIP)`"
+        v-show="searchStatus"
+        v-model="wip"
+        @change="filter_by_name"
+      ></v-switch>
       <v-text-field
         hide-details
         single-line
@@ -86,9 +92,18 @@ export default {
       router.push({ name: "project", params: { project_id: id } });
     },
     filter_by_name(){
-      this.projects = this.init_projects.filter(project => project.name.toUpperCase().indexOf(this.search_input.toUpperCase()) != -1);
+      this.projects = this.init_projects.filter(
+        project => (
+          project.name.toUpperCase().indexOf(
+            !this.search_input ? "" : this.search_input.toUpperCase()
+          ) != -1
+          &&
+          ( this.wip != project.wip || ! this.wip )
+        )
+       );
       this.selected_tags = [];
     },
+
     add_filter_tag(tag){
       if (this.selected_tags.indexOf(tag) == -1){
       this.selected_tags.push(tag);
